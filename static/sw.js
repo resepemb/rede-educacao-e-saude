a75fadbc24dc03155b3597d3f441c78af1943aee
Gabriel Café <63719840+GabrielCoffee9@users.noreply.github.com>
2024-03-05 13:50:57 -0300

    Built site for gh-pages

---
+++ static/sw.js
@@ -0,0 +1,23 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
+
+const { registerRoute } = workbox.routing;
+const { precacheAndRoute } = workbox.precaching;
+
+registerRoute(
+    '/\.(?:xlsx|json)$/',
+    new workbox.strategies.CacheFirst({
+        "cacheName": 'assets',
+        plugins: [
+            new workbox.expiration.ExpirationPlugin({
+                maxEntries: 60,
+                maxAgeSeconds: 30 * 24 * 60 * 60,
+            })
+        ]
+    })
+);
+
+precacheAndRoute([
+    {url: '/index.html', revision: '01'},
+    {url: '/404.html', revision: '01'}
+]
+);
